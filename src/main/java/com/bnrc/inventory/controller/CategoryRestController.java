3162a608e7faa26415e57d8eb87fac3819ee4715
unknown <benjaminrojas824@gmail.com>
2023-12-07 05:38:33 -0300

    Guardar categorias

--- src/main/java/com/bnrc/inventory/controller/CategoryRestController.java
+++ src/main/java/com/bnrc/inventory/controller/CategoryRestController.java
@@ -4,8 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bnrc.inventory.model.Category;
 import com.bnrc.inventory.response.CategoryResponseRest;
 import com.bnrc.inventory.services.ICategoryService;
 
@@ -29,5 +33,13 @@ public class CategoryRestController {
 		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
 		return response;
 	}
+	
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+		  
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		return response;
+	}
+	
 
 }
--- src/main/java/com/bnrc/inventory/services/ICategoryService.java
+++ src/main/java/com/bnrc/inventory/services/ICategoryService.java
@@ -2,6 +2,7 @@ package com.bnrc.inventory.services;
 
 import org.springframework.http.ResponseEntity;
 
+import com.bnrc.inventory.model.Category;
 import com.bnrc.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
@@ -9,5 +10,7 @@ public interface ICategoryService {
 	public ResponseEntity<CategoryResponseRest> search();
 	
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
+	
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
